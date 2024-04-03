2d24770e233596973e620db038b61145d9e9de78
Genia Melnik <geniasheiko@gmail.com>
2024-04-03 15:26:46 +0300

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Header = () => {
+   return <header className='header'>
+      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTd-lkijlO0g1F6MHKlfo2erxZ-z8FSlzTHRA&s' />
+   </header>
+}
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+   return <div className='content'>
+      <div>
+         <img src='https://t4.ftcdn.net/jpg/02/56/10/07/360_F_256100731_qNLp6MQ3FjYtA3Freu9epjhsAj2cwU9c.jpg' />
+      </div>
+      <div>
+         ava+description
+      </div>
+      <div>
+         My posts
+      </div>
+      <div>
+         New posts
+      </div>
+      <div>
+         <div>
+            post1
+         </div>
+         <div>
+            post2
+         </div>
+      </div>
+   </div>
+
+
+}
+export default Profile
