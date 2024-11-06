1a581f808bcce8112944e761c38233a1f1d8e359
Genia Melnyk <geniasheiko@gmail.com>
2024-11-06 16:05:41 +0200

    Updates

---
+++ precache-manifest.ae3f8878feb4f403cc552f58a0a53630.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a86ee54ef7ba45f9196f5d0157e54700",
+    "url": "/way-samyray-1/index.html"
+  },
+  {
+    "revision": "6d81a40fcc42aeaa5a26",
+    "url": "/way-samyray-1/static/css/main.18412206.chunk.css"
+  },
+  {
+    "revision": "0ff725af014519c1b449",
+    "url": "/way-samyray-1/static/js/2.6f62aa61.chunk.js"
+  },
+  {
+    "revision": "2e57d6a55bcbb71f8165",
+    "url": "/way-samyray-1/static/js/3.bfeeef29.chunk.js"
+  },
+  {
+    "revision": "6d81a40fcc42aeaa5a26",
+    "url": "/way-samyray-1/static/js/main.3dc66791.chunk.js"
+  },
+  {
+    "revision": "1d028c2e13466db66eb6",
+    "url": "/way-samyray-1/static/js/runtime~main.b6597a6e.js"
+  },
+  {
+    "revision": "d0bbebca32c9a7c6dc23d5154e22674c",
+    "url": "/way-samyray-1/static/media/preloader.d0bbebca.svg"
+  }
+]);
